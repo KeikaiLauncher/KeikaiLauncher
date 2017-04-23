1d5650294a884ad72638c6b873601d7bc7f4c104
Avuton Olrich <avuton@gmail.com>
2017-04-23 11:02:55 -0700

    AboutActivity: This changes the About user interface to be more simple.
    This commit is an attempt to simplify the About screen, while not compromising
    the information given to the user.

--- app/src/main/java/com/hayaisoftware/launcher/activities/AboutActivity.java
+++ app/src/main/java/com/hayaisoftware/launcher/activities/AboutActivity.java
@@ -15,32 +15,94 @@
 package com.hayaisoftware.launcher.activities;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.Menu;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 import com.hayaisoftware.launcher.BuildConfig;
 import com.hayaisoftware.launcher.R;
 
+/**
+ * This class is a user interface to provide simple information about the current build.
+ */
 public class AboutActivity extends Activity {
 
+    /**
+     * This method returns a link to a Github user profile.
+     *
+     * @param userName The Github username for the profile. If null, this method will return the
+     *                 {@code name} parameter with no url.
+     * @param name     The actual name of the user for the Github profile.
+     * @return A link to the Github user profile for the user detailed in the parameters.
+     */
+    private static CharSequence getGithubUserUrl(final CharSequence userName,
+                                                 final CharSequence name) {
+        final CharSequence results;
+
+        if (userName == null) {
+            results = name;
+        } else {
+            results = getUrl("https://www.github.com/" + userName, name);
+        }
+
+        return results;
+    }
+
+    /**
+     * This method returns a {@link Html} link.
+     *
+     * @param url  The HTML Url for this link.
+     * @param name The HTML Name for this link.
+     * @return The entire HTML url as {@link Html}.
+     */
+    private static CharSequence getUrl(final CharSequence url, final CharSequence name) {
+        final String sb = "<a href=\"" + url + "\">" + name + "</a>";
+
+        return Html.fromHtml(sb);
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        TextView aboutTextView=((TextView) findViewById(R.id.about_textview));
-        aboutTextView.setText(aboutTextView.getText().toString().replace("[APP_VERSION]",
-                BuildConfig.VERSION_NAME));
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
-    }
+        final Resources resources = getResources();
+        final String[] contributorUserNames =
+                resources.getStringArray(R.array.about_contributor_usernames);
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        finish();
+        // Setup the project name/url
+        final CharSequence projectUrl = getString(R.string.about_project_website_url);
+        final CharSequence projectMaintainer = contributorUserNames[0];
+        final TextView projectUrlView = ((TextView) findViewById(R.id.about_project_website_url));
+
+        projectUrlView.setText(getUrl(projectUrl, "github.com/" + projectMaintainer));
+        projectUrlView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // Set the version
+        final TextView version = ((TextView) findViewById(R.id.about_version));
+
+        version.setText(BuildConfig.VERSION_NAME);
+
+        // Setup the license name/url
+        final String licenseUrl = getString(R.string.about_license_url);
+        final String licenseName = getString(R.string.about_license_type);
+        final TextView licenseType = ((TextView) findViewById(R.id.license_type));
+
+        licenseType.setText(getUrl(licenseUrl, licenseName));
+        licenseType.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // Setup the contributor names/urls.
+        final String[] contributorNames =
+                resources.getStringArray(R.array.about_contributor_names);
+        final TextView contributorView = (TextView) findViewById(R.id.about_contributors);
+
+        for (int i = 0; i < contributorNames.length; i++) {
+            contributorView.append(getGithubUserUrl(contributorUserNames[i], contributorNames[i]));
+            contributorView.append(System.getProperty("line.separator"));
+        }
+
+        contributorView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
