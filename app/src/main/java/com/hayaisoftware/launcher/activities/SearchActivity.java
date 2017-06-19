KeikaiLauncher/KeikaiLauncher

commit e872a2b07b29ed9aba7a83286fbf43cc4caa618b

PackageChangedReceiver: Refactor PackageChangedReceiver.
This commit:

* Adds the receiver to the manifest, and enables it at runtime, statically.

* Adds external storage support and package suspending (API 24+).

* Changes behaviour from abusing the Preferences callback to using our own
custom callback.

* Fixes many issues when things appear and disappear in different
circumstances, including when the list is being filtered.

app/src/main/java/com/hayaisoftware/launcher/activities/SearchActivity.java
@@ -16,9 +16,8 @@ package com.hayaisoftware.launcher.activities;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
-import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
@@ -30,6 +29,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -52,11 +53,13 @@ import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.hayaisoftware.launcher.BuildConfig;
 import com.hayaisoftware.launcher.LaunchableActivity;
 import com.hayaisoftware.launcher.LaunchableActivityPrefs;
 import com.hayaisoftware.launcher.LaunchableAdapter;
 import com.hayaisoftware.launcher.LoadLaunchableActivityTask;
-import com.hayaisoftware.launcher.PackageChangedReceiver;
+import com.hayaisoftware.launcher.monitor.PackageChangeCallback;
+import com.hayaisoftware.launcher.monitor.PackageChangedReceiver;
 import com.hayaisoftware.launcher.R;
 import com.hayaisoftware.launcher.ShortcutNotificationManager;
 import com.hayaisoftware.launcher.fragments.SettingsFragment;
@@ -68,13 +71,13 @@ import java.util.Collection;
 import static com.hayaisoftware.launcher.util.ContentShare.getLaunchableResolveInfos;
 
 public class SearchActivity extends Activity
-        implements SharedPreferences.OnSharedPreferenceChangeListener {
+        implements SharedPreferences.OnSharedPreferenceChangeListener, PackageChangeCallback {
 
+    private static final String TAG = "SearchActivity";
     private static final String SEARCH_EDIT_TEXT_KEY = "SearchEditText";
     private LaunchableAdapter<LaunchableActivity> mAdapter;
     private SharedPreferences mSharedPreferences;
     private EditText mSearchEditText;
-    private BroadcastReceiver mPackageChangedReceiver;
     private InputMethodManager mInputMethodManager;
     private View mOverflowButtonTopleft;
 
@@ -95,6 +98,17 @@ public class SearchActivity extends Activity
         }
 
         return hasNavBar;
+    }
+
+    public static Collection<ResolveInfo> getLaunchableResolveInfos(final PackageManager pm,
+            @Nullable final String activityName) {
+        final Intent intent = new Intent();
+
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.setPackage(activityName);
+
+        return pm.queryIntentActivities(intent, 0);
     }
 
     /**
@@ -158,6 +172,57 @@ public class SearchActivity extends Activity
     }
 
     /**
+     * Synchronize to this lock when the Adapter is visible and might be called by multiple
+     * threads.
+     */
+    private final Object mLock = new Object();
+
+    /**
+     * Called when a package appears for any reason.
+     *
+     * @param activityName The name of the {@link Activity} of the package which appeared.
+     */
+    @Override
+    public void onPackageAppeared(final String activityName) {
+        final PackageManager pm = getPackageManager();
+        final Iterable<ResolveInfo> resolveInfos = getLaunchableResolveInfos(pm, activityName);
+
+        synchronized (mLock) {
+            if (mAdapter.getClassNamePosition(activityName) == -1) {
+                addToAdapter(resolveInfos);
+                mAdapter.sortApps();
+                updateFilter(mSearchEditText.getText());
+            }
+        }
+    }
+
+    /**
+     * Called when a package disappears for any reason.
+     *
+     * @param activityName The name of the {@link Activity} of the package which disappeared.
+     */
+    @Override
+    public void onPackageDisappeared(final String activityName) {
+        synchronized (mLock) {
+            mAdapter.removeAllByName(activityName);
+            updateFilter(mSearchEditText.getText());
+        }
+    }
+
+    /**
+     * Called when an existing package is updated or its disabled state changes.
+     *
+     * @param activityName The name of the {@link Activity} of the package which was modified.
+     */
+    @Override
+    public void onPackageModified(final String activityName) {
+        synchronized (mLock) {
+            onPackageDisappeared(activityName);
+            onPackageAppeared(activityName);
+        }
+    }
+
+    /**
      * This method returns the size of the dimen
      *
      * @param resources The resources for the containing the named identifier.
@@ -212,14 +277,8 @@ public class SearchActivity extends Activity
 
         setupPadding(transparentPossible && noMultiWindow);
 
-        final IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_PACKAGE_ADDED);
-        filter.addAction(Intent.ACTION_PACKAGE_CHANGED);
-        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
-        filter.addAction(Intent.ACTION_PACKAGE_REPLACED);
-        filter.addDataScheme("package");
-        mPackageChangedReceiver = new PackageChangedReceiver();
-        registerReceiver(mPackageChangedReceiver, filter);
+        PackageChangedReceiver.setCallback(this);
+        enableReceiver();
 
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         setupPreferences();
@@ -227,6 +286,23 @@ public class SearchActivity extends Activity
 
         //loadShareableApps();
         setupViews();
+    }
+
+    private void enableReceiver() {
+        final PackageManager pm  = getPackageManager();
+        final ComponentName componentName = new ComponentName(this, PackageChangedReceiver.class);
+
+        pm.setComponentEnabledSetting(componentName,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    private void disableReceiver() {
+        final ComponentName name = new ComponentName(this, PackageChangedReceiver.class);
+
+        getPackageManager().setComponentEnabledSetting(name,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
     }
 
     /**
@@ -285,6 +361,14 @@ public class SearchActivity extends Activity
         }
     }
 
+    private void updateFilter(final CharSequence cs) {
+        final int seqLength = cs.length();
+
+        if (seqLength != 1 || cs.charAt(0) != '\0') {
+            mAdapter.getFilter().filter(cs);
+        }
+    }
+
     private EditText setupSearchEditText() {
         final EditText searchEditText = findViewById(R.id.user_search_input);
 
@@ -294,14 +378,10 @@ public class SearchActivity extends Activity
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                final int seqLength = s.length();
-
-                if (seqLength != 1 || s.charAt(0) != '\0') {
-                    mAdapter.getFilter().filter(s);
-                }
+                updateFilter(s);
 
                 // Avoid performing visible app update
-                mClearButton.setVisibility(seqLength > 0 ? View.VISIBLE : View.GONE);
+                mClearButton.setVisibility(s.length() > 0 ? View.VISIBLE : View.GONE);
             }
 
             @Override
@@ -462,31 +542,40 @@ public class SearchActivity extends Activity
             adapter = loadLaunchableApps();
         } else {
             adapter = new LaunchableAdapter<>(object, this, R.layout.app_grid_item);
+            adapter.setNotifyOnChange(true);
         }
 
         return adapter;
     }
 
+    private void addToAdapter(@NonNull final Iterable<ResolveInfo> infoList) {
+        final PackageManager pm = getPackageManager();
+        final String thisCanonicalName = getClass().getCanonicalName();
+
+        for (final ResolveInfo info : infoList) {
+            // Don't include activities from this package.
+            if (!thisCanonicalName.startsWith(info.activityInfo.packageName)) {
+                final String label = info.activityInfo.loadLabel(pm).toString();
+                final LaunchableActivity launchableActivity =
+                        new LaunchableActivity(info.activityInfo, label, false);
+
+                mAdapter.add(launchableActivity);
+            }
+        }
+    }
+
     private LaunchableAdapter<LaunchableActivity> loadLaunchableApps() {
         final PackageManager pm = getPackageManager();
-        final Collection<ResolveInfo> infoList = getLaunchableResolveInfos(pm);
+        final Collection<ResolveInfo> infoList = getLaunchableResolveInfos(pm, null);
         final int infoListSize = infoList.size();
         final LaunchableAdapter<LaunchableActivity> adapter
                 = new LaunchableAdapter<>(this, R.layout.app_grid_item, infoListSize);
         final int cores = Runtime.getRuntime().availableProcessors();
-        final String thisCanonicalName = getClass().getCanonicalName();
 
         if (cores <= 1) {
-            for (final ResolveInfo info : infoList) {
-                // Don't include activities from this package.
-                if (!thisCanonicalName.startsWith(info.activityInfo.packageName)) {
-                final LaunchableActivity launchableActivity = new LaunchableActivity(
-                        info.activityInfo, info.activityInfo.loadLabel(pm).toString(), false);
-
-                    adapter.add(launchableActivity);
-                }
-            }
+            addToAdapter(infoList);
         } else {
+            final String thisCanonicalName = getClass().getCanonicalName();
             final SimpleTaskConsumerManager simpleTaskConsumerManager =
                     new SimpleTaskConsumerManager(cores, infoListSize);
 
@@ -523,48 +612,6 @@ public class SearchActivity extends Activity
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 
-    private void handlePackageChanged() {
-        final SharedPreferences.Editor editor = mSharedPreferences.edit();
-        final String[] packageChangedNames = mSharedPreferences.getString("package_changed_name", "")
-                .split(" ");
-        final Intent intent = new Intent(Intent.ACTION_MAIN);
-
-        editor.remove("package_changed_name");
-        editor.apply();
-
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        for (String packageName : packageChangedNames) {
-            packageName = packageName.trim();
-
-            if (!packageName.isEmpty()) {
-                intent.setPackage(packageName);
-                final Collection<ResolveInfo> infoList =
-                        getPackageManager().queryIntentActivities(intent, 0);
-
-                if (infoList.isEmpty()) {
-                    Log.d("SearchActivity",
-                            "No activities in list. Uninstall detected: " + packageName);
-                    mAdapter.remove(packageName);
-                } else {
-                    Log.d("SearchActivity", "Activities in list. Install/update detected!");
-                    final PackageManager pm = getPackageManager();
-
-                    for (final ResolveInfo info : infoList) {
-                        if (mAdapter.getPosition(info.activityInfo.packageName) == -1) {
-                            final LaunchableActivity launchableActivity = new LaunchableActivity(
-                                    info.activityInfo, info.activityInfo.loadLabel(pm).toString(),
-                                    false);
-                            mAdapter.add(launchableActivity);
-                        }
-                    }
-                }
-            }
-        }
-
-        mAdapter.sortApps();
-    }
-
     @Override
     public void onBackPressed() {
         if (isCurrentLauncher()) {
@@ -576,10 +623,10 @@ public class SearchActivity extends Activity
 
     @Override
     protected void onDestroy() {
-        unregisterReceiver(mPackageChangedReceiver);
         if (!isChangingConfigurations()) {
             Log.d("HayaiLauncher", "Hayai is ded");
         }
+        disableReceiver();
         mAdapter.onDestroy();
         super.onDestroy();
     }
@@ -595,9 +642,7 @@ public class SearchActivity extends Activity
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         //does this need to run in uiThread?
-        if (key.equals("package_changed_name") && !sharedPreferences.getString(key, "").isEmpty()) {
-            handlePackageChanged();
-        } else if (key.equals("pref_app_preferred_order")) {
+        if (key.equals("pref_app_preferred_order")) {
             setPreferredOrder();
             mAdapter.sortApps();
         } else if (key.equals("pref_disable_icons")) {
@@ -759,12 +804,22 @@ public class SearchActivity extends Activity
             launchableActivity.setLaunchTime();
             launchableActivity.addUsage();
             prefs.writePreference(launchableActivity);
-            mAdapter.sortApps();
-        } catch (ActivityNotFoundException e) {
-            //this should only happen when the launcher still hasn't updated the file list after
-            //an activity removal.
-            Toast.makeText(this, getString(R.string.activity_not_found),
-                    Toast.LENGTH_SHORT).show();
+
+            if (mAdapter.isOrderedByRecent()) {
+                mAdapter.sort(LaunchableAdapter.RECENT);
+            } else if (mAdapter.isOrderedByUsage()) {
+                mAdapter.sort(LaunchableAdapter.USAGE);
+            }
+        } catch (final ActivityNotFoundException e) {
+            if (BuildConfig.DEBUG) {
+                throw e;
+            } else {
+                final String notFound = getString(R.string.activity_not_found);
+
+                Log.e(TAG, notFound, e);
+                Toast.makeText(this, getString(R.string.activity_not_found),
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     }
 

app/src/main/AndroidManifest.xml
@@ -50,5 +50,23 @@
             android:name=".activities.AboutActivity"
             android:label="@string/about_header"
             android:theme="@android:style/Theme.DeviceDefault.Light.Dialog"/>
+
+        <receiver
+            android:name=".monitor.PackageChangedReceiver"
+            android:enabled="false">
+            <intent-filter>
+                <action android:name="android.intent.action.PACKAGE_ADDED"/>
+                <action android:name="android.intent.action.PACKAGE_CHANGED"/>
+                <action android:name="android.intent.action.PACKAGE_REMOVED"/>
+                <action android:name="android.intent.action.PACKAGE_REPLACED"/>
+                <data android:scheme="package"/>
+            </intent-filter>
+            <intent-filter>
+                <action android:name="android.intent.action.EXTERNAL_APPLICATIONS_AVAILABLE"/>
+                <action android:name="android.intent.action.EXTERNAL_APPLICATIONS_UNAVAILABLE"/>
+                <action android:name="android.intent.action.PACKAGES_SUSPENDED"/>
+                <action android:name="android.intent.action.PACKAGES_UNSUSPENDED"/>
+            </intent-filter>
+        </receiver>
     </application>
 </manifest>

app/src/main/java/com/hayaisoftware/launcher/LaunchableAdapter.java
@@ -15,11 +15,13 @@
 package com.hayaisoftware.launcher;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,7 +42,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -54,28 +55,30 @@ public class LaunchableAdapter<T extends LaunchableActivity> extends BaseAdapter
     /**
      * This comparator orders {@link LaunchableActivity} objects in alphabetical order.
      */
-    private static final Comparator<LaunchableActivity> ALPHABETICAL = new AlphabeticalOrder();
-
-    private static final Pattern DIACRITICAL_MARKS =
-            Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+    public static final Comparator<LaunchableActivity> ALPHABETICAL = new AlphabeticalOrder();
 
     /**
      * This comparator orders {@link LaunchableActivity} objects with "pins" at the head of the
      * list.
      */
-    private static final Comparator<LaunchableActivity> PIN_TO_TOP = new PinToTop();
+    public static final Comparator<LaunchableActivity> PIN_TO_TOP = new PinToTop();
 
     /**
      * This comparator orders {@link LaunchableActivity} objects in most recently used at the head
      * of the list.
      */
-    private static final Comparator<LaunchableActivity> RECENT = new RecentOrder();
+    public static final Comparator<LaunchableActivity> RECENT = new RecentOrder();
 
     /**
      * This comparator orders {@link LaunchableActivity} objects in most used order at the head
      * of the list.
      */
-    private static final Comparator<LaunchableActivity> USAGE = new UsageOrder();
+    public static final Comparator<LaunchableActivity> USAGE = new UsageOrder();
+
+    private static final Pattern DIACRITICAL_MARKS =
+            Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+
+    private static final String TAG = "LaunchableAdapter";
 
     /**
      * The context of the parent object.
@@ -346,6 +349,34 @@ public class LaunchableAdapter<T extends LaunchableActivity> extends BaseAdapter
     }
 
     /**
+     * Returns the position of a {@link LaunchableActivity} where the
+     * {@link LaunchableActivity#getComponent()}.{@link ComponentName#getClassName()} is equal to
+     * the {@code className} parameter.
+     *
+     * @param className The classname to find.
+     * @return The LaunchableActivity matching the classname parameter, {@code -1} if not found.
+     */
+    public int getClassNamePosition(@NonNull final String className) {
+        final List<T> current;
+        int position = -1;
+
+        if (mOriginalValues == null) {
+            current = mObjects;
+        } else {
+            current = mOriginalValues;
+        }
+
+        final int currentSize = current.size();
+        for (int i = 0; i < currentSize && position == -1; i++) {
+            if (current.get(i).getComponent().getClassName().equals(className)) {
+                position = i;
+            }
+        }
+
+        return position;
+    }
+
+    /**
      * Returns the context associated with this array adapter. The context is used
      * to create views from the resource passed to the constructor.
      *
@@ -422,12 +453,13 @@ public class LaunchableAdapter<T extends LaunchableActivity> extends BaseAdapter
 
     /**
      * Returns the position of a {@link LaunchableActivity} where the
-     * {@link LaunchableActivity#getClassName()} is equal to the {@code className} parameter.
+     * {@link LaunchableActivity#getComponent()}.{@link ComponentName#getPackageName()} is
+     * equal to the {@code className} parameter.
      *
-     * @param className The classname to find.
+     * @param packageName The package name to find in this adapter.
      * @return The LaunchableActivity matching the classname parameter, {@code -1} if not found.
      */
-    public int getPosition(@NonNull final String className) {
+    public int getPackageNamePosition(@NonNull final String packageName) {
         final List<T> current;
         int position = -1;
 
@@ -437,10 +469,10 @@ public class LaunchableAdapter<T extends LaunchableActivity> extends BaseAdapter
             current = mOriginalValues;
         }
 
-        for (int i = 0; i < current.size(); i++) {
-            if (className.equals(current.get(i).getClassName())) {
+        final int currentSize = current.size();
+        for (int i = 0; i < currentSize && position == -1; i++) {
+            if (current.get(i).getComponent().getPackageName().equals(packageName)) {
                 position = i;
-                break;
             }
         }
 
@@ -468,7 +500,10 @@ public class LaunchableAdapter<T extends LaunchableActivity> extends BaseAdapter
         }
 
         view.setVisibility(View.VISIBLE);
-        final LaunchableActivity launchableActivity = getItem(position);
+        final LaunchableActivity launchableActivity;
+        synchronized (mLock) {
+            launchableActivity = getItem(position);
+        }
         final CharSequence label = launchableActivity.getActivityLabel();
         final TextView appLabelView = view.findViewById(R.id.appLabel);
         final ImageView appIconView = view.findViewById(R.id.appIcon);
@@ -525,12 +560,21 @@ public class LaunchableAdapter<T extends LaunchableActivity> extends BaseAdapter
         }
     }
 
+    public boolean isOrderedByRecent() {
+        return mOrderByRecent;
+    }
+
+    public boolean isOrderedByUsage() {
+        return mOrderByUsage;
+    }
+
     /**
      * Notifies the attached observers that the underlying data has been changed
      * and any View reflecting the data set should refresh itself.
      */
     @Override
     public void notifyDataSetChanged() {
+        Log.v(TAG, "Notifying about adapter change");
         super.notifyDataSetChanged();
         mNotifyOnChange = true;
     }
@@ -546,44 +590,25 @@ public class LaunchableAdapter<T extends LaunchableActivity> extends BaseAdapter
         }
     }
 
-    /**
-     * This method removes all applications belonging to a package name.
-     *
-     * @param packageName The package name to remove.
-     * @return Whether the adapter was modified.
-     */
-    public boolean remove(@NonNull final String packageName) {
-        final int initCount;
-        final int resultCount;
-        final Iterator<T> iter;
+    public boolean remove(final int index) {
+        final List<T> current;
+        final T result;
 
         synchronized (mLock) {
             if (mOriginalValues == null) {
-                iter = mObjects.listIterator();
-                initCount = mObjects.size();
+                current = mObjects;
             } else {
-                iter = mOriginalValues.listIterator();
-                initCount = mOriginalValues.size();
+                current = mOriginalValues;
             }
 
-            while (iter.hasNext()) {
-                if (iter.next().getClassName().startsWith(packageName)) {
-                    iter.remove();
-                }
-            }
-
-            if (mOriginalValues == null) {
-                resultCount = mObjects.size();
-            } else {
-                resultCount = mOriginalValues.size();
-            }
+            result = current.remove(index);
         }
 
         if (mNotifyOnChange) {
             notifyDataSetChanged();
         }
 
-        return initCount != resultCount;
+        return result != null;
     }
 
     /**
@@ -603,6 +628,64 @@ public class LaunchableAdapter<T extends LaunchableActivity> extends BaseAdapter
         if (mNotifyOnChange) {
             notifyDataSetChanged();
         }
+    }
+
+    /**
+     * This method removes all items by name.
+     *
+     * This method removes all items by name. This method has been made very specifically to meet
+     * the need of this Adapter.
+     *
+     * This method will remove a package name if it has an equal {@code name} in this Adapter.
+     *
+     * This method will remove a class name if it has a classname that starts with {@code name} in
+     * this adapter.
+     *
+     * This complexity is required because some packages install multiple activities (see "A Photo
+     * Manager"
+     *
+     * Some packages install activities with duplicate names (see Google Drive/Google Sheets).
+     *
+     * This method should not be part of this class, but we rely on locking the collections during
+     * this critical method.
+     *
+     * @param name The name. See the description for more information.
+     * @return The number of packages removed by this method.
+     */
+    public int removeAllByName(@NonNull final String name) {
+        ComponentName component;
+        final List<T> current;
+        int removedCount = 0;
+
+        synchronized (mLock) {
+            if (mOriginalValues == null) {
+                current = mObjects;
+            } else {
+                current = mOriginalValues;
+            }
+
+            for (int i = current.size() - 1; i >= 0; i--) {
+                //noinspection ConstantConditions
+                component = current.get(i).getComponent();
+
+                if (component.getClassName().startsWith(name)) {
+                    Log.d(TAG, "Removing " + name +
+                            " by starting with classname: " + component.getClassName());
+                    current.remove(i);
+                    removedCount++;
+                } else if (component.getPackageName().equals(name)) {
+                    Log.d(TAG, "Found position of " + name);
+                    current.remove(i);
+                    removedCount++;
+                }
+            }
+        }
+
+        if (mNotifyOnChange) {
+            notifyDataSetChanged();
+        }
+
+        return removedCount;
     }
 
     /**
@@ -695,11 +778,11 @@ public class LaunchableAdapter<T extends LaunchableActivity> extends BaseAdapter
         }
     }
 
-    /*
+    /**
      * Returns a string representation of the current LaunchActivity collection.
      *
-             * @return A string representation of the current LaunchActivity collection.
-            */
+     * @return A string representation of the current LaunchActivity collection.
+     */
     @Override
     public String toString() {
         final String toString;

commit 73664587c30d692c4d3cf07220c8ad080a2da275

SearchActivity: Fix adding to the adapter on single core devices.
This fixes a regression in commit e872a2b.

app/src/main/java/com/hayaisoftware/launcher/activities/SearchActivity.java
@@ -187,7 +187,7 @@ public class SearchActivity extends Activity
 
         synchronized (mLock) {
             if (mAdapter.getClassNamePosition(activityName) == -1) {
-                addToAdapter(resolveInfos);
+                addToAdapter(mAdapter, resolveInfos);
                 mAdapter.sortApps();
                 updateFilter(mSearchEditText.getText());
             }
@@ -498,7 +498,8 @@ public class SearchActivity extends Activity
         return adapter;
     }
 
-    private void addToAdapter(@NonNull final Iterable<ResolveInfo> infoList) {
+    private void addToAdapter(final LaunchableAdapter<LaunchableActivity> adapter,
+            @NonNull final Iterable<ResolveInfo> infoList) {
         final PackageManager pm = getPackageManager();
         final String thisCanonicalName = getClass().getCanonicalName();
 
@@ -508,7 +509,7 @@ public class SearchActivity extends Activity
                 final LaunchableActivity launchableActivity =
                         LaunchableActivity.getLaunchable(info.activityInfo, pm);
 
-                mAdapter.add(launchableActivity);
+                adapter.add(launchableActivity);
             }
         }
     }
@@ -522,7 +523,7 @@ public class SearchActivity extends Activity
         final int cores = Runtime.getRuntime().availableProcessors();
 
         if (cores <= 1) {
-            addToAdapter(infoList);
+            addToAdapter(adapter, infoList);
         } else {
             final String thisCanonicalName = getClass().getCanonicalName();
             final SimpleTaskConsumerManager simpleTaskConsumerManager =